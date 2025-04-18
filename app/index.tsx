fdb7c26b5702d107369a8dbc10b8c90d203902d4
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-18 13:14:01 +0200

    Update styles in index component to use width and height for container layout

--- app/index.tsx
+++ app/index.tsx
@@ -14,7 +14,8 @@ export default function index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: "100%",
+    height: "100%",
     justifyContent: "center",
     alignItems: "center",
   },
