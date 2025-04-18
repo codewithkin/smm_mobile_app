cb659fd7f3d2c79d1e4bba5e00cd5f60ad2a0cec
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-18 13:11:48 +0200

    Refactor layout and index components, remove unused files and hooks

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={styles.heading}>Login to Smart Switch Mobile</Text>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: "600",
+    textAlign: "center"
+  }
+});
