fb6a7499db35e95d8e58b94b22d50a8df6695866
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-19 12:47:10 +0200

    Refactor Dashboard layout to use ScrollView for improved scrolling behavior and enhance data presentation with updated gradient cards

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import TopBar from "../../components/TopBar";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 import { colors } from "@/constants/colors";
 import { MotiView } from "moti";
 import { useEffect, useState } from "react";
@@ -37,93 +37,106 @@ export default function Dashboard() {
         height: "100%",
       }}
     >
-      <View style={styles.container}>
-        <TopBar />
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+        style={{
+          backgroundColor: colors.primary,
+          height: "100%",
+        }}
+      >
+        <View style={styles.container}>
+          <TopBar />
 
-        {/* Home content with animation */}
-        <MotiView
-          from={{ opacity: 0, translateY: 200 }}
-          animate={{ opacity: 1, translateY: 0 }}
-          transition={{ type: "timing", duration: 1000 }}
-          style={styles.content}
-        >
-          {loading ? (
-            <View style={styles.skeletonContainer}>
-              <View style={styles.skeletonCard} />
-              <View style={styles.skeletonCard} />
-              <View style={styles.skeletonCard} />
-            </View>
-          ) : (
-            <View style={styles.dataContainer}>
-              <View style={styles.twinGrid}>
-                {/* Customers */}
-                <LinearGradient
-                  colors={[colors.red, colors.purple]}
-                  style={[styles.gradientCard, { width: "47%" }]}
-                >
-                  {/* Icon */}
-                  <FontAwesome6 name="bag-shopping" size={32} color="white" />
-
-                  {/* Number */}
-                  {data ? (
-                    <Text style={styles.dataKey}>{data?.customers.length}</Text>
-                  ) : (
-                    <Text style={styles.dataKey}>0</Text>
-                  )}
-
-                  {/* Title */}
-                  <Text style={styles.cardHeading}>Customers</Text>
-                </LinearGradient>
-
-                {/* Products */}
-                <LinearGradient
-                  colors={[colors.turquoise, colors.green]}
-                  style={[styles.gradientCard, { width: "47%" }]}
-                >
-                  {/* Icon */}
-                  <FontAwesome6 name="bag-shopping" size={32} color="white" />
-
-                  {/* Number */}
-                  {data ? (
-                    <Text style={styles.dataKey}>{data?.products.length}</Text>
-                  ) : (
-                    <Text style={styles.dataKey}>0</Text>
-                  )}
-
-                  {/* Title */}
-                  <Text style={styles.cardHeading}>Products</Text>
-                </LinearGradient>
+          {/* Home content with animation */}
+          <MotiView
+            from={{ opacity: 0, translateY: 200 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ type: "timing", duration: 1000 }}
+            style={styles.content}
+          >
+            {loading ? (
+              <View style={styles.skeletonContainer}>
+                <View style={styles.skeletonCard} />
+                <View style={styles.skeletonCard} />
+                <View style={styles.skeletonCard} />
               </View>
+            ) : (
+              <View style={styles.dataContainer}>
+                <View style={styles.twinGrid}>
+                  {/* Customers */}
+                  <LinearGradient
+                    colors={[colors.red, colors.purple]}
+                    style={[styles.gradientCard, { width: "47%" }]}
+                  >
+                    {/* Icon */}
+                    <FontAwesome6 name="bag-shopping" size={32} color="white" />
 
-              {/* All-time sales */}
-              <LinearGradient
-                colors={[colors.purple, colors.secondary]}
-                style={styles.gradientCard}
-              >
-                {/* Icon */}
-                <MaterialCommunityIcons
-                  name="cash-check"
-                  size={32}
-                  color="white"
-                />
+                    {/* Number */}
+                    {data ? (
+                      <Text style={styles.dataKey}>
+                        {data?.customers.length}
+                      </Text>
+                    ) : (
+                      <Text style={styles.dataKey}>0</Text>
+                    )}
 
-                {/* Number */}
-                {data ? (
-                  <Text style={styles.dataKey}>{data?.checkouts.length}</Text>
-                ) : (
-                  <Text style={styles.dataKey}>0</Text>
-                )}
+                    {/* Title */}
+                    <Text style={styles.cardHeading}>Customers</Text>
+                  </LinearGradient>
 
-                {/* Title */}
-                <Text style={styles.cardHeading}>Sales</Text>
-              </LinearGradient>
+                  {/* Products */}
+                  <LinearGradient
+                    colors={[colors.turquoise, colors.green]}
+                    style={[styles.gradientCard, { width: "47%" }]}
+                  >
+                    {/* Icon */}
+                    <FontAwesome6 name="bag-shopping" size={32} color="white" />
 
-              {/* Line chart showing sales */}
-              {data.checkouts && <SalesLineChart sales={data.checkouts} />}
-            </View>
-          )}
-        </MotiView>
-      </View>
+                    {/* Number */}
+                    {data ? (
+                      <Text style={styles.dataKey}>
+                        {data?.products.length}
+                      </Text>
+                    ) : (
+                      <Text style={styles.dataKey}>0</Text>
+                    )}
+
+                    {/* Title */}
+                    <Text style={styles.cardHeading}>Products</Text>
+                  </LinearGradient>
+                </View>
+
+                {/* All-time sales */}
+                <LinearGradient
+                  colors={[colors.purple, colors.secondary]}
+                  style={styles.gradientCard}
+                >
+                  {/* Icon */}
+                  <MaterialCommunityIcons
+                    name="cash-check"
+                    size={32}
+                    color="white"
+                  />
+
+                  {/* Number */}
+                  {data ? (
+                    <Text style={styles.dataKey}>{data?.checkouts.length}</Text>
+                  ) : (
+                    <Text style={styles.dataKey}>0</Text>
+                  )}
+
+                  {/* Title */}
+                  <Text style={styles.cardHeading}>Sales</Text>
+                </LinearGradient>
+
+                {/* Line chart showing sales */}
+                {data.checkouts && <SalesLineChart sales={data.checkouts} />}
+              </View>
+            )}
+          </MotiView>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
