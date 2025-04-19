1afdfd1673212910ca5b8e11884a8adf9791a8a6
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-19 09:35:41 +0200

    Add Customers, Inventory, and Receipts pages with basic layout

---
+++ app/(tabs)/customers/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function CustomersPage() {
+  return (
+    <View>
+      <Text>Customers Page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
---
+++ app/(tabs)/inventory/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function InventoryPage() {
+  return (
+    <View>
+      <Text>Inventory Page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
