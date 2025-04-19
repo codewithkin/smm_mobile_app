6a0a9b1b3dd36fdc52d6047905a212d8a2d105d9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-19 14:08:05 +0200

    Update backend URL for production environment in urls.ts

--- constants/urls.ts
+++ constants/urls.ts
@@ -1,6 +1,6 @@
 export const urls = {
   backendUrl:
     process.env.NODE_ENV === "production"
-      ? ""
+      ? "https://smm-server-uj4q.onrender.com/api/smm"
       : "http://192.168.1.2:8080/api/smm",
 };
